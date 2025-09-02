bb8ff598e8a7caab956a6209731a6d4a024bc7c8
Kubilay <kubilaybee@gmail.com>
2025-09-02 16:01:44 +0300

    feat: RestController, GetMapping, PathVariable use

--- src/main/java/com/springprojects/restful_web_services/RestfulWebServicesApplication.java
+++ src/main/java/com/springprojects/restful_web_services/RestfulWebServicesApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestfulWebServicesApplication.class, args);
-		System.out.println("Restful Web Services Start !!");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebServicesApplication.class, args);
+        System.out.println("Restful Web Services Start !!");
+    }
 
 }
