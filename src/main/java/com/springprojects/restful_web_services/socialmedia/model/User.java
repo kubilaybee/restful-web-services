3f0ae17a3f1771e6d0a82ed37605118432c66cac
Kubilay <kubilaybee@gmail.com>
2025-09-05 00:40:39 +0300

    feat: static filtering example added

--- src/main/java/com/springprojects/restful_web_services/socialmedia/model/User.java
+++ src/main/java/com/springprojects/restful_web_services/socialmedia/model/User.java
@@ -1,5 +1,6 @@
 package com.springprojects.restful_web_services.socialmedia.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
@@ -7,8 +8,10 @@ import java.time.LocalDate;
 public class User {
     private Integer id;
     @Size(min = 2, message = "Name should have at least 2 characters")
+    @JsonProperty("first_name")
     private String name;
     @Past(message = "Birth date should me in the past")
+    @JsonProperty("birth_date")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
